21e08de6023208dc7ad7344ecba822077dc51860
fabiovenicius <fabiovenicius@yahoo.com.br>
2020-06-18 11:51:43 -0300

    Implementação do Map

--- fundamentos/map_2.js
+++ fundamentos/map_2.js
@@ -1,1 +1,16 @@
 //Implementação do MAP
+Array.prototype.map2 = function(cb){
+    const novoArray = []
+    for(let i = 0; i < this.length; i++){
+        novoArray.push(cb(this[i]))
+    }
+    return novoArray
+}
+
+//Teste
+
+nums = [1,2,3,4,5,6]
+
+const dobro = nums.map2(valor=>valor * 2)
+
+console.log(dobro)
